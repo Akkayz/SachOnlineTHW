be330c81d90a2b64042393d269d8e06f5cc31494
Akkay <nguyenhung.dev.info@gmail.com>
2023-10-10 16:58:01 +0700

    - trước khi test

---
+++ SachOnlineTHW/SachOnline/SachOnline/Controllers/GioHangController.cs
@@ -0,0 +1,78 @@
+﻿using SachOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class GioHangController : Controller
+    {
+        private dbSachOnlineDataContext db = new dbSachOnlineDataContext(ConfigurationManager.ConnectionStrings["SachOnlineConnectionString"].ConnectionString);
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public List<GioHang> LayGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        public ActionResult ThemGioHang(int ms, string url)
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            GioHang sp = lstGioHang.Find(n => n.iMaSach == ms);
+            if (sp == null)
+            {
+                sp = new GioHang(ms);
+                lstGioHang.Add(sp);
+            }
+            else
+            {
+                sp.iSoLuong++;
+            }
+            return Redirect(url);
+        }
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                iTongSoLuong = lstGioHang.Sum(n => n.iSoLuong);
+
+            }
+            return iTongSoLuong;
+        }
+        private double TongTien()
+        {
+            double dTongTien = 0;
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang != null)
+            {
+                dTongTien = lstGioHang.Sum(n => n.dThanhTien);
+            }
+            return dTongTien;
+        }
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGioHang = LayGioHang();
+            if (lstGioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "SachOnline");
+
+            }
+            ViewBag.TongSoLuong = TongSoLuong();
+            ViewBag.TongTien = TongTien();
+            return View(lstGioHang);
+        }
+    }
+}   
