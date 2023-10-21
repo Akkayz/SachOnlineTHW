d900352c381550feeb82f3c8e226fb44d1ce4d80
khanh1133 <2124801030252@student.tdmu.edu.vn>
2023-10-21 13:49:57 +0700

    Done1,2

---
+++ SachOnline/Areas/Admin/Controllers/SachController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+using System.Configuration;
+
+namespace SachOnline.Areas.Admin.Controllers
+
+{
+    public class SachController : Controller
+
+    {
+
+        private dbSachOnlineDataContext db = new dbSachOnlineDataContext(ConfigurationManager.ConnectionStrings["SachOnlineConnectionString"].ConnectionString);
+
+        // GET: Admin/Sach
+
+        public ActionResult Index(int ? page)
+
+        {
+            int iPageNum = (page ?? 1);
+            int iPageSize = 7;
+            return View(db.SACHes.ToList().OrderBy(n => n.MaSach).ToPagedList(iPageNum,iPageSize));
+
+        }
+
+    }
+
+}
