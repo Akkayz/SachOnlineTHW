e22b46de99ffdd52a24cd72c9ae1b0ed303479a2
khanh1133 <2124801030252@student.tdmu.edu.vn>
2023-10-21 14:41:20 +0700

    Done 3

--- SachOnline/Areas/Admin/Controllers/SachController.cs
+++ SachOnline/Areas/Admin/Controllers/SachController.cs
@@ -8,6 +8,8 @@ using PagedList;
 using PagedList.Mvc;
 using System.IO;
 using System.Configuration;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 namespace SachOnline.Areas.Admin.Controllers
 
@@ -20,15 +22,71 @@ namespace SachOnline.Areas.Admin.Controllers
 
         // GET: Admin/Sach
 
-        public ActionResult Index(int ? page)
+        public ActionResult Index(int? page)
 
         {
             int iPageNum = (page ?? 1);
             int iPageSize = 7;
-            return View(db.SACHes.ToList().OrderBy(n => n.MaSach).ToPagedList(iPageNum,iPageSize));
+            return View(db.SACHes.ToList().OrderBy(n => n.MaSach).ToPagedList(iPageNum, iPageSize));
 
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
 
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(SACH sach, FormCollection f, HttpPostedFileBase fFileUpload)
+        {
+            ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe");
+            ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+
+
+
+            if (fFileUpload == null)
+            {
+
+                //Néi. dung théng béo yéu cau chon anh bia
+
+                ViewBag.ThongBao = "Hãy chọn ảnh bìa";
+                ViewBag.TenSach = f["sTenSach"];
+                ViewBag.MoTa = f["sMoTa"];
+                ViewBag.SoLuong = int.Parse(f["iSoLuong"]);
+                ViewBag.GiaBan = decimal.Parse(f["mGiaBan"]);
+                ViewBag.MaCD = new SelectList(db.CHUDEs.ToList().OrderBy(n => n.TenChuDe), "MaCD", "TenChuDe", int.Parse(f["MaCD"]));
+                ViewBag.MaNXB = new SelectList(db.NHAXUATBANs.ToList().OrderBy(n => n.TenNXB), "MaNXB", "TenNXB", int.Parse(f["MaNXB"]));
+
+                return View();
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var sFileName = Path.GetFileName(fFileUpload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images"), sFileName);
+                    if (!System.IO.File.Exists(path))
+                    {
+                        fFileUpload.SaveAs(path);
+                    }
+                    sach.TenSach = f["sTenSach"];
+                    sach.MoTa = f["sMoTa"];
+                    sach.AnhBia = sFileName;
+                    sach.NgayCapNhat = Convert.ToDateTime(f["dNgayCapNhat"]);
+                    sach.SoLuongBan = int.Parse(f["iSoLuong"]);
+                    sach.GiaBan = decimal.Parse(f["mGiaBan"]);
+                    sach.MaCD = int.Parse(f["MaCD"]);
+                    sach.MaNXB = int.Parse(f["MaNXB"]);
+                    db.SACHes.InsertOnSubmit(sach);
+                    db.SubmitChanges();
+
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+        }
     }
-
 }
